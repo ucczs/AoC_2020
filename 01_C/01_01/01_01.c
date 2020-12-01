4e956e3fa31364385e77da840da109868ab6b27a
DESKTOP-8MGJ15I\Faraday <aron-freyer@gmx.de>
2020-12-01 06:19:36 +0100

    First day finished

--- 01_C/01_01/01_01.c
+++ 01_C/01_01/01_01.c
@@ -5,15 +5,19 @@
 #define TEST_RUN 0
 
 #if( TEST_RUN == 1 )
-    #define LINE_LENGTH 10
+    #define LINE_LENGTH 6
+    #define NUMBER_TOTAL 6
 #else
-    #define LINE_LENGTH 30
+    #define LINE_LENGTH 6
+    #define NUMBER_TOTAL 200
 #endif
 
 
 int main(int argc, char *argv[]) {
     FILE* fp;
     char line[LINE_LENGTH];
+    int numbers[NUMBER_TOTAL];
+    int idx = 0;
 
 #if( TEST_RUN == 1 )
     fp = fopen("D:\\Creativity\\Advent_of_Code\\AoC_2020\\01_C\\01_01\\test.txt", "r");
@@ -29,12 +33,22 @@ int main(int argc, char *argv[]) {
 
     while(fgets(line, sizeof(line), fp) != NULL)
     {
-        printf("%s", line);
+        numbers[idx] = atoi(line);
+
+        idx++;
     }
 
-
-
-
+    for (int firstNo = 0; firstNo < NUMBER_TOTAL; firstNo++)
+    {
+        for (int secondNo = firstNo+1; secondNo < NUMBER_TOTAL; secondNo++)
+        {
+            if (numbers[firstNo] + numbers[secondNo] == 2020)
+                {
+                    int result = numbers[firstNo] * numbers[secondNo];
+                    printf("Result: %d\n",result);
+                }
+        }
+    }
 
     return(0);
 }
--- 01_C/01_02/01_02.c
+++ 01_C/01_02/01_02.c
@@ -2,23 +2,27 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define TEST_RUN 1
+#define TEST_RUN 0
 
 #if( TEST_RUN == 1 )
-    #define LINE_LENGTH 10
+    #define LINE_LENGTH 6
+    #define NUMBER_TOTAL 6
 #else
-    #define LINE_LENGTH 30
+    #define LINE_LENGTH 6
+    #define NUMBER_TOTAL 200
 #endif
 
 
 int main(int argc, char *argv[]) {
     FILE* fp;
     char line[LINE_LENGTH];
+    int numbers[NUMBER_TOTAL];
+    int idx = 0;
 
 #if( TEST_RUN == 1 )
-    fp = fopen("D:\\Creativity\\Advent_of_Code\\AoC_2020\\01_C\\01_02\\test.txt", "r");
+    fp = fopen("D:\\Creativity\\Advent_of_Code\\AoC_2020\\01_C\\01_01\\test.txt", "r");
 #else
-    fp = fopen("D:\\Creativity\\Advent_of_Code\\AoC_2020\\01_C\\01_02\\input.txt", "r");
+    fp = fopen("D:\\Creativity\\Advent_of_Code\\AoC_2020\\01_C\\01_01\\input.txt", "r");
 #endif
 
     if(fp == NULL)
@@ -29,10 +33,25 @@ int main(int argc, char *argv[]) {
 
     while(fgets(line, sizeof(line), fp) != NULL)
     {
-        printf("%s", line);
+        numbers[idx] = atoi(line);
+
+        idx++;
     }
 
-
+    for (int firstNo = 0; firstNo < NUMBER_TOTAL; firstNo++)
+    {
+        for (int secondNo = firstNo+1; secondNo < NUMBER_TOTAL; secondNo++)
+        {
+            for (int thirdNo = secondNo+1; thirdNo < NUMBER_TOTAL; thirdNo++)
+            {
+            if (numbers[firstNo] + numbers[secondNo] + numbers[thirdNo] == 2020)
+                {
+                    int result = numbers[firstNo] * numbers[secondNo] * numbers[thirdNo];
+                    printf("Result: %d\n",result);
+                }
+            }
+        }
+    }
 
     return(0);
 }
