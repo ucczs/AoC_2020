c2a47a01766fe9692644616d59f4f05f9380c757
DESKTOP-8MGJ15I\Faraday <aron-freyer@gmx.de>
2020-12-03 07:19:01 +0100

    Day 3 completed

---
+++ 03_C/03_01/03_01.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define TEST_RUN 0
+
+#if( TEST_RUN == 1 )
+    #define LINE_LENGTH 13
+    #define FOREST_DOWN_SIZE 11
+    #define FOREST_WIDTH 11
+#else
+    #define LINE_LENGTH 34
+    #define FOREST_DOWN_SIZE 323
+    #define FOREST_WIDTH 31
+#endif
+
+#define SLOPE_COUNT 5
+
+#define STEP_SIZE_RIGHT 3
+#define STEP_SIZE_DOWN 1
+
+
+int getCollisions(bool map[FOREST_DOWN_SIZE][FOREST_WIDTH])
+{
+    int collision_count = 0;
+    int count_right = 0;
+    int count_down = 0;
+
+    for (int i = 0; i < (FOREST_DOWN_SIZE / STEP_SIZE_DOWN)-1; i++)
+    {
+        count_down += STEP_SIZE_DOWN;
+        count_right += STEP_SIZE_RIGHT;
+        count_right = count_right % FOREST_WIDTH;
+
+        if (map[count_down][count_right] == true)
+        {
+            collision_count++;
+        }
+    }
+
+    return(collision_count);
+}
+
+void readInMap(char* line, bool map[FOREST_DOWN_SIZE][FOREST_WIDTH])
+{
+    static int depth_count = 0;
+
+    printf("%s", line);
+
+    for (int i = 0; i < FOREST_WIDTH; i++)
+    {
+        if( line[i] == '.')
+        {
+            map[depth_count][i] = false;
+        }
+        else if( line[i] == '#')
+        {
+            map[depth_count][i] = true;
+        }
+    }
+
+    depth_count++;
+}
+
+int main(int argc, char *argv[]) {
+    FILE* fp;
+    char line[LINE_LENGTH];
+    bool map[FOREST_DOWN_SIZE][FOREST_WIDTH];
+
+#if( TEST_RUN == 1 )
+    fp = fopen("D:\\Creativity\\Advent_of_Code\\AoC_2020\\03_C\\03_01\\test.txt", "r");
+#else
+    fp = fopen("D:\\Creativity\\Advent_of_Code\\AoC_2020\\03_C\\03_01\\input.txt", "r");
+#endif
+
+    if(fp == NULL)
+    {
+        printf("File could not be opened!");
+        exit(1);
+    }
+
+    while(fgets(line, sizeof(line), fp) != NULL)
+    {
+        readInMap(&line[0], map);
+    }
+
+    int result = getCollisions(map);
+
+    printf("\n\nNumber of collisions: %d", result);
+
+    fclose(fp);
+    return(0);
+}
---
+++ 03_C/03_02/03_02.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define TEST_RUN 0
+
+#if( TEST_RUN == 1 )
+    #define LINE_LENGTH 13
+    #define FOREST_DOWN_SIZE 11
+    #define FOREST_WIDTH 11
+#else
+    #define LINE_LENGTH 34
+    #define FOREST_DOWN_SIZE 323
+    #define FOREST_WIDTH 31
+#endif
+
+#define SLOPE_COUNT 5
+
+int slope_right[SLOPE_COUNT];
+int slope_down[SLOPE_COUNT];
+
+
+int defineSlopes()
+{
+    slope_right[0] = 1;
+    slope_down[0] = 1;
+
+    slope_right[1] = 3;
+    slope_down[1] = 1;
+
+    slope_right[2] = 5;
+    slope_down[2] = 1;
+
+    slope_right[3] = 7;
+    slope_down[3] = 1;
+
+    slope_right[4] = 1;
+    slope_down[4] = 2;
+}
+
+void getCollisionsWithDifferentSlopes(bool map[FOREST_DOWN_SIZE][FOREST_WIDTH], int collision_collection[SLOPE_COUNT])
+{
+    for (int  j = 0; j < SLOPE_COUNT; j++)
+    {
+        int collision_count = 0;
+        int count_right = 0;
+        int count_down = 0;
+
+        if (map[count_down][count_right] == true)
+        {
+            collision_count++;
+        }
+
+        for (int i = 0; i < (FOREST_DOWN_SIZE / slope_down[j])-1; i++)
+        {
+            count_down += slope_down[j];
+            count_right += slope_right[j];
+            count_right = count_right % FOREST_WIDTH;
+
+            if (map[count_down][count_right] == true)
+            {
+                collision_count++;
+            }
+            
+        }
+        collision_collection[j] = collision_count;
+    }
+}
+
+long long int getCollisionMultiplikation(int collision_collection[SLOPE_COUNT])
+{
+    long long int result = collision_collection[0];
+    for (int i = 1; i < SLOPE_COUNT; i++)
+    {
+        result *= collision_collection[i];
+    }
+
+    return(result);
+}
+
+void readInMap(char* line, bool map[FOREST_DOWN_SIZE][FOREST_WIDTH])
+{
+    static int depth_count = 0;
+
+    printf("%s", line);
+
+    for (int i = 0; i < FOREST_WIDTH; i++)
+    {
+        if( line[i] == '.')
+        {
+            map[depth_count][i] = false;
+        }
+        else if( line[i] == '#')
+        {
+            map[depth_count][i] = true;
+        }
+    }
+
+    depth_count++;
+}
+
+int main(int argc, char *argv[]) {
+    FILE* fp;
+    char line[LINE_LENGTH];
+    bool map[FOREST_DOWN_SIZE][FOREST_WIDTH];
+
+#if( TEST_RUN == 1 )
+    fp = fopen("D:\\Creativity\\Advent_of_Code\\AoC_2020\\03_C\\03_02\\test.txt", "r");
+#else
+    fp = fopen("D:\\Creativity\\Advent_of_Code\\AoC_2020\\03_C\\03_02\\input.txt", "r");
+#endif
+
+    if(fp == NULL)
+    {
+        printf("File could not be opened!");
+        exit(1);
+    }
+
+    while(fgets(line, sizeof(line), fp) != NULL)
+    {
+        readInMap(&line[0], map);
+    }
+
+    defineSlopes();
+
+    int collision_collection[SLOPE_COUNT];
+    getCollisionsWithDifferentSlopes(map, collision_collection);
+
+    long long int result = getCollisionMultiplikation(collision_collection);
+    printf("\n\nNumber of collisions: %lld", result);
+
+    fclose(fp);
+    return(0);
+}
